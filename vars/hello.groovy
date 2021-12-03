db6adcd90670feb66f6663497307446f1508bc03
darshanbm099 <93187003+darshanbm099@users.noreply.github.com>
2021-12-03 15:58:43 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,6 +1,5 @@
 def call(){
   sh '''
-  echo hello world
   curl -X POST -v -u $GitCred_USR:$GitCred_PSW -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/darshanbm99/$apiname -d '{"scm": "git", "is_private": "true","project": {"key": "'JEN'"} }'
 
   '''
