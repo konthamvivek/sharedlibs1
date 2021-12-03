acb775f668b225724b4b6476909e3117676526c8
darshanbm099 <93187003+darshanbm099@users.noreply.github.com>
2021-12-03 14:44:57 +0530

    Update repocreate.groovy

--- vars/repocreate.groovy
+++ vars/repocreate.groovy
@@ -1,5 +1,5 @@
 def  repocreate(GitCred_USR,GitCred_PSW){
     sh '''
-    curl -X POST -v -u $GitCred_USR:$GitCred_PSW -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/$GitCred_USR/$apiname -d '{"scm": "git", "is_private": "true","project": {"key": "'VIV'"} }'   
+    curl -X POST -v -u $GitCred_USR:$GitCred_PSW -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/$GitCred_USR/$apiname -d '{"scm": "git", "is_private": "true","project": {"key": "'JEN'"} }'   
     '''
 }
