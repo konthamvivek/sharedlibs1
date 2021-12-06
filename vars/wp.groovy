44b2298df8b60660384cf2a99c07db1dce54a2ed
darshanbm099 <93187003+darshanbm099@users.noreply.github.com>
2021-12-06 11:48:09 +0530

    Update wp.groovy

--- vars/wp.groovy
+++ vars/wp.groovy
@@ -1,4 +1,4 @@
-def create(param){
+def create(){
   sh '''
   
   curl -X POST -v -u $GitCred_USR:$GitCred_PSW -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/$GitCred_USR/$apiname -d '{"scm": "git", "is_private": "true","project": {"key": "'$param'"} }'
