1b8a1f4a07107d0fb1fccd4aec04abbd62a0c671
darshanbm099 <93187003+darshanbm099@users.noreply.github.com>
2021-12-06 11:45:02 +0530

    Update gtrepo.groovy

--- vars/gtrepo.groovy
+++ vars/gtrepo.groovy
@@ -1,7 +1,7 @@
 def gtcreate(){
   sh '''
     ls -la
-    curl -u $GitCred_USR:$GitCred_PSW https://api.github.com/user/repos -d '{"name":"'$apiName'","private":true}'
+    curl -u $GitCred_USR:$GitCred_PSW https://api.github.com/user/repos -d '{"name":"'$apiname'","private":true}'
     
   '''
 }
