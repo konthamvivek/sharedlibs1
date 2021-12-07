e5d737cde270ca7bdd50301ec2758f1c19578d0a
darshanbm099 <93187003+darshanbm099@users.noreply.github.com>
2021-12-07 12:20:02 +0530

    Update createrepo.groovy

--- vars/createrepo.groovy
+++ vars/createrepo.groovy
@@ -1,18 +1,45 @@
-def repo(String name){
-  if name.equals("github"){
-  sh '''
-    ls -la
-    curl -u $GitCred_USR:$GitCred_PSW https://api.github.com/user/repos -d '{"name":"'$apiname'","private":true}'
-    
-  '''
-}
-  else if name.equals("bitbucket"){
 
-  sh '''
+def create(){
+  if (place.equals("bitbucket") {
+    pipeline {
+      agent any
+      environment{
+          GitCred=credentials('bbtdarshan')
+      }
+      stages {
+        stage('bitbucket') {
+          steps {
+            script{
+            echo "The build number is for bitbucket"
+              sh '''
   
-  curl -X POST -v -u $GitCred_USR:$GitCred_PSW -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/$GitCred_USR/$apiname -d '{"scm": "git", "is_private": "true","project": {"key": "'JEN'"} }'
+              curl -X POST -v -u $GitCred_USR:$GitCred_PSW -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/$GitCred_USR/$apiname -d '{"scm": "git", "is_private": "true","project": {"key": "'$project_name'"} }'
   
-  '''
-}
-  
+               '''
+            }
+          }
+        }
+      }
+    }
+  }
+  else {
+    pipeline {
+      agent any
+        environment{
+          GitCred=credentials('github')
+      }
+      stages {
+        stage('github') {
+          steps {
+            echo "The build number is for github"
+              sh '''
+                ls -la
+                curl -u $GitCred_USR:$GitCred_PSW https://api.github.com/user/repos -d '{"name":"'$apiname'","private":true}'
+    
+                '''
+          }
+        }
+      }
+    }
+  }
 }
