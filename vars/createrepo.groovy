5ba7d41bc33acded345eb49618bbc93a83dc99d7
darshanbm099 <93187003+darshanbm099@users.noreply.github.com>
2021-12-07 12:48:56 +0530

    Update createrepo.groovy

--- vars/createrepo.groovy
+++ vars/createrepo.groovy
@@ -1,22 +1,41 @@
-def call(int buildNumber) {
-  if (buildNumber % 2 == 0) {
+def create(place){
+  if (place.equals("bitbucket")) {
     pipeline {
       agent any
+      environment{
+          GitCred=credentials('bbtdarshan')
+      }
       stages {
-        stage('Even Stage') {
+        stage('bitbucket') {
           steps {
-            echo "The build number is even"
+            script{
+            
+              sh '''
+  
+              curl -X POST -v -u $GitCred_USR:$GitCred_PSW -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/$GitCred_USR/$apiname -d '{"scm": "git", "is_private": "true","project": {"key": "'$project_name'"} }'
+  
+               '''
+            }
           }
         }
       }
     }
-  } else {
+  }
+  else {
     pipeline {
       agent any
+        environment{
+          GitCred=credentials('github')
+      }
       stages {
-        stage('Odd Stage') {
+        stage('github') {
           steps {
-            echo "The build number is odd"
+            
+              sh '''
+                ls -la
+                curl -u $GitCred_USR:$GitCred_PSW https://api.github.com/user/repos -d '{"name":"'$apiname'","private":true}'
+    
+                '''
           }
         }
       }
